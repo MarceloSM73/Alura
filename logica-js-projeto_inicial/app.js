afe6fd6e37f73a9a9f44ce150be02c029b4bb1c7
Marcelo Soares Moura <marcelosmoura@hotmail.com>
2023-11-30 16:03:22 -0300

    add

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+let contador = prompt('Escolha um número:');
+
+while (contador <= 0) {
+  
+  // mostrando no console
+  console.log('Executando a interação ' + contador);
+
+  //contador = contador + 1;
+  contador--
+}
