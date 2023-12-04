0b7d846f946c9e1f7c550a6ccf278a9f670ddaf7
Marcelo Soares Moura <marcelosmoura@hotmail.com>
2023-12-04 09:55:59 -0300

    add

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,11 @@
-let contador = prompt('Escolha um número:');
+let numero = prompt('Escolha um número:');
+let contador = 0
 
-while (contador <= 0) {
+while (contador <= numero) {
   
   // mostrando no console
-  console.log('Executando a interação ' + contador);
+  console.log('Executando a interação: ' + contador);
 
   //contador = contador + 1;
-  contador--
+  contador++
 }
