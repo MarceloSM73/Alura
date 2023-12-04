a42dfa30b864ede25d0b987b2f0e7f6d29fc4501
Marcelo Soares Moura <marcelosmoura@hotmail.com>
2023-12-04 16:20:25 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,35 @@
-let numero = prompt('Escolha um número:');
-let contador = 0
+alert('Seja bem-vindo ao Jogo do Número Screto!');
 
-while (contador <= numero) {
-  
-  // mostrando no console
-  console.log('Executando a interação: ' + contador);
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+console.log(`O código secreto é: ${numeroSecreto}`);
 
-  //contador = contador + 1;
-  contador++
+let chute;
+let tentativas = 1;
+console.log(`Seu chute foi: ${chute}`);
+console.log(`Resultado da comparação: ${chute == numeroSecreto}`);
+
+// while => enquanto (condição)
+while (chute != numeroSecreto){
+    chute = prompt('Informe um número entre 1 e 100: ');
+   
+    if (chute == numeroSecreto) {
+      break;
+    
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
+    // tentativas = tentativas +1
+    tentativas++;
 }
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns! Você descobriu o numero secreto  ${numeroSecreto}, com ${tentativas} ${palavraTentativa}!`);
+
+//if (tentativas > 1){
+//  alert(`Parabéns! Você descobriu o numero secreto  ${numeroSecreto}, com ${tentativas} tentativas!`);
+//} else {
+//    alert(`Parabéns! Você descobriu o numero secreto  ${numeroSecreto}, com ${tentativas} tentativa!`);
+//}
