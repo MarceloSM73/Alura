8f09eb459965bf99d3a949f00d1dbb13a71cf771
Marcelo Soares Moura <marcelosmoura@hotmail.com>
2023-12-20 15:40:59 -0300

    Update app.js

--- html/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ html/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,10 +1,5 @@
-// let titulo = document.querySelector("h1");
-// titulo.innerHTML = "Jogo do Número Secreto"
-
-// let paragrafo = document.querySelector("p");
-// paragrafo.innerHTML = "Escolha um número entre 1 e 10:";
-
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -15,19 +10,19 @@ exibirTextoNaTela("h1", "Jogo do Número Secreto");
 exibirTextoNaTela("p", "Escolha um número entre 1 e 10:");
 
 function verificarChute(){
-    //corpo da função
-    //console.log("O botão foi clicado.")
-    let chute = document.querySelector("input").value;
-    //console.log(chute == numeroSecreto);
+        let chute = document.querySelector("input").value;
     if (chute == numeroSecreto){
         exibirTextoNaTela("h1", "Acertou!");
-        exibirTextoNaTela("p", "Você descobriu o número secreto!");
+        let palavraTentativa = tentativas >1 ? "tentativas" : "tentativa";
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela("p", mensagemTentativas);
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela("p", "O número secreto é menor");
         } else {
             exibirTextoNaTela("p", "O número secreto é maior");
         }
+        tentativas++;
     }
 }
 
