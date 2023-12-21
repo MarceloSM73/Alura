f9088f15b541ca88a2e022ad4a8de69f384640b7
Marcelo Soares Moura <marcelosmoura@hotmail.com>
2023-12-21 16:50:33 -0300

    update app

--- html/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ html/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,38 +1,10 @@
-let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 1;
-
-function exibirTextoNaTela(tag, texto){
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
-}
-
-exibirTextoNaTela("h1", "Jogo do Número Secreto");
-exibirTextoNaTela("p", "Escolha um número entre 1 e 10:");
-
-function verificarChute(){
-        let chute = document.querySelector("input").value;
-    if (chute == numeroSecreto){
-        exibirTextoNaTela("h1", "Acertou!");
-        let palavraTentativa = tentativas >1 ? "tentativas" : "tentativa";
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
-        exibirTextoNaTela("p", mensagemTentativas);
-        document.getElementById("reiniciar").removeAttribute("disabled");
-    } else {
-        if (chute > numeroSecreto) {
-            exibirTextoNaTela("p", "O número secreto é menor");
-        } else {
-            exibirTextoNaTela("p", "O número secreto é maior");
-        }
-        tentativas++;
-        limparCampo();
+function calculoFatorial(){
+    let num = prompt("Digite um número para calcular o seu fatorial");
+    while (num>0){
+        num = num*fatorial;
+        fatorial--;
+    return num;
     }
+    alert(`O fatorial de ${num} é ${fatorial}.`);
 }
-
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
-}
-
-function limparCampo() {
-    chute = document.querySelector("input");
-    chute.value = "";
-}
+calculoFatorial();
