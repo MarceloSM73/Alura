74501c96fb00aa5889abc20da37731bc1d169ae7
MarceloSM73 <marcelosmoura@hotmail.com>
2024-01-15 21:49:53 -0300

    Create desafiosArrays.js

---
+++ Desafios 3/desafiosArrays.js
@@ -0,0 +1,21 @@
+//Lista vazia
+let listaGenerica = [];
+
+//Lista de linguagens de programação
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+//Adicionando linguagens
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+console.log(linguagensDeProgramacao); // Saída: ["JavaScript", "C", "C++", "Kotlin", "Python", "Java", "Ruby", "GoLang"]
+
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaNomes = ["João", "Maria", "José"];
+console.log(listaNomes[0]);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let listaNomes = ["João", "Maria", "José"];
+console.log(listaNomes[1]);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let listaNomes = ["João", "Maria", "José"];
+console.log(listaNomes[listaNomes.length -1]);
